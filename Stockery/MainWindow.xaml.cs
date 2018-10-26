bb1fe5c4dbd2aacb79619ded4a876eea2f6961e0
Otto Rinaldo <otto.rinaldo@gmail.com>
2018-10-26 18:35:02 +0200

    Added onPropertyChanged
    - MainViewModel
    - ViewModelBase

--- Stockery/MainWindow.xaml.cs
+++ Stockery/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Stockery.ViewModel;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Stockery
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private MainViewModel _mainViewModel;
+
+        public MainWindow(MainViewModel mainViewModel)
         {
             InitializeComponent();
+            _mainViewModel = mainViewModel;
+            DataContext = mainViewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Stockery/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Stockery.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Stockery.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private IStockDataService _stockDataService;
+        private Stock _selectedStock;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel(IStockDataService stockDataService)
+        {
+            Stocks = new ObservableCollection<Stock>();
+            _stockDataService = stockDataService;
+        }
+
+        public void Load()
+        {
+            var stocks = _stockDataService.GetStocks();
+            Stocks.Clear();
+            foreach (var stock in stocks)
+            {
+                Stocks.Add(stock);
+            }
+        }
+
+        public ObservableCollection<Stock> Stocks { get; set; }
+
+        public Stock SelectedStock
+        {
+            get { return _selectedStock; }
+            set
+            {
+                _selectedStock = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
